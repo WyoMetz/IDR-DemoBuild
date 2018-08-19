2f193c133dd30afb73721ac423e500932a636f6a
WyoMetz <34138193+WyoMetz@users.noreply.github.com>
2018-08-19 00:41:44 -0400

    Comments for all ConnectionReader functions.

--- IDR Demo build/ConnectionReader.cs
+++ IDR Demo build/ConnectionReader.cs
@@ -9,30 +9,33 @@ public class ConnectionReader
 {
 	private static List<string> BackgroundImage()
 	{
-		string UserName = Environment.UserName.ToString();
-		string filePath = @"C:\Users\"+UserName+ @"\AppData\Local\IDR\ResourceLocations.txt";
-		string fileLocation = Path.GetFullPath(filePath);
-		using (var streamReader = new StreamReader(fileLocation))
+		string UserName = Environment.UserName.ToString();										//Gets the Computers logged in Username
+
+		string filePath = @"C:\Users\"+UserName+ @"\AppData\Local\IDR\ResourceLocations.txt";	//finds the Resource file
+		
+		string fileLocation = Path.GetFullPath(filePath);										//Corrects the path to file
+
+		using (var streamReader = new StreamReader(fileLocation))								//Reads the lines within the text file
 		{
 			List<string> outputValues = new List<string>();
 			while (!streamReader.EndOfStream)
 			{
 				var lines = streamReader.ReadLine();
-				var values = lines.Split(';');
+				var values = lines.Split(';');													// The resources are defined as such e.g. BackgroundLocation; (String to background)
 
 				if(values[0] == "BackgroundLocation")
 				{
-					string lineOutput = values[1];
+					string lineOutput = values[1];												 //The String Location is all we want
 
 					outputValues.Add(lineOutput);
 				}
 			}
-			return outputValues;
+			return outputValues;																//returns the String as a List<string> to the calling function
 		}
 	}
-	private static string ImageString = BackgroundImage().First();
+	private static string ImageString = BackgroundImage().First();								// we then define the string statically
 	/// <summary>
 	/// The URI of the background Image defined by ResourceLocations.txt
 	/// </summary>
-	public Uri ImageUri = new Uri(ImageString, UriKind.RelativeOrAbsolute);
+	public Uri ImageUri = new Uri(ImageString, UriKind.RelativeOrAbsolute);						// pass that to the URI generator to give us a proper URI for use by the Image Class.
 }
