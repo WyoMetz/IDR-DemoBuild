526502a8979ff5bd5c4ecff3f01aee2022d14ef4
WyoMetz <34138193+WyoMetz@users.noreply.github.com>
2018-12-01 17:57:19 -0500

    Began Work on the expanded CSV Reader

---
+++ FunctionTest/ExpnadedCsvReader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionTest
+{
+	class ExpnadedCsvReader
+	{
+		private static string CsvHeader;
+
+		public static Task ReadCsv(string FullFilePath)
+		{
+			using(StreamReader reader = new StreamReader(FullFilePath))
+			{
+
+			}
+		}
+	}
+}
